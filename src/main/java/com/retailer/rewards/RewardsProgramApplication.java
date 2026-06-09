3da5af0ccc0aacd65c4cbe3361c57e5d9a283574
Tejas Nambiar <tejasnambiar32@gmail.com>
2026-06-09 21:32:08 +0530

    Initial commit

---
+++ src/main/java/com/retailer/rewards/RewardsProgramApplication.java
@@ -0,0 +1,13 @@
+package com.retailer.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardsProgramApplication.class, args);
+    }
+
+}
